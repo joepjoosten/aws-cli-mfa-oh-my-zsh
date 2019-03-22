a990b06f74754de5b356612aa3367c212114b462
Joep Joosten <joep.joosten@ihomer.nl>
2019-03-22 22:16:40 +0100

    Fixed ah-my-zsh install scripts

--- aws-mfa.plugin.zsh
+++ aws-mfa.plugin.zsh
@@ -1,3 +1,3 @@
-source ~/.oh-my-zsh/custom/plugins/aws-cli-mfa/getaws
-source ~/.oh-my-zsh/custom/plugins/aws-cli-mfa/clearaws
+source ~/.oh-my-zsh/custom/plugins/aws-mfa/getaws
+source ~/.oh-my-zsh/custom/plugins/aws-mfa/clearaws
 alias aws-mfa='getaws'
